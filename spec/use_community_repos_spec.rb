7b093c91b4c498f570ed97172bf455ffca114ea8
cn <cn@mycrobase.de>
2013-04-22 19:57:10 +0200

    Finally fix Chef11 compat

--- spec/use_community_repos_spec.rb
+++ spec/use_community_repos_spec.rb
@@ -7,20 +7,20 @@ describe 'prosody::use_community_repos' do
   end
 
   let(:chef_run) do
-    chef_runner.converge 'prosody::use_community_repos'
+    chef_runner.converge 'apt', 'prosody::use_community_repos'
   end
   
   it 'creates prosody-lenny list on Debian 5' do
     chef_runner.node.automatic_attrs['platform'] = 'debian'
     chef_runner.node.automatic_attrs['platform_version'] = '5.0.0'
-    chef_run = chef_runner.converge 'prosody::use_community_repos'
+    chef_run = chef_runner.converge 'apt', 'prosody::use_community_repos'
     expect(chef_run).to create_file "/etc/apt/sources.list.d/prosody-lenny.list"
   end
   
   it 'creates prosody-squeeze list on Debian 6' do
     chef_runner.node.automatic_attrs['platform'] = 'debian'
     chef_runner.node.automatic_attrs['platform_version'] = '6.0.0'
-    chef_run = chef_runner.converge 'prosody::use_community_repos'
+    chef_run = chef_runner.converge 'apt', 'prosody::use_community_repos'
     expect(chef_run).to create_file "/etc/apt/sources.list.d/prosody-squeeze.list"
   end
 end
